c4fcd7cf60ad50717fc9e3e122e85f48865f5cf9
grbllcnls <gabriellecanlas6@gmail.com>
2024-03-12 12:38:55 +0800

    Added social links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { FaGithub, FaLinkedin, FaDeviantart } from "react-icons/fa";
+import gabImage from './images/me.jpg';
+import backgroundImage from './images/laundromat.gif'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <header className="header">
+      <div className="gab-container">
+        <img 
+          src={gabImage} 
+          alt="Gabrielle" 
+          className="gab-image"
+        />
+      <div class="container">
+      <div class="pixel"><p>Gabrielle Canlas</p></div>
+     
+      <div className="social-links">
+        <a href="https://github.com/grbllcnls" target="_blank"><FaGithub size={30} /></a>
+        <a href="https://www.linkedin.com/in/gabrielle-canlas/" target="_blank"><FaLinkedin size={30} /></a>
+        <a href="https://www.deviantart.com/gbrllcnls" target="_blank"><FaDeviantart size={30} /></a>
+      </div>
+      </div>
+      </div>
+    </header>
+  </div>
   );
-}
+  }
+
 
 export default App;
